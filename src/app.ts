455f86cba0d80e14724f79f2f8886ab759d04c75
Jonas Lacandola <jonaslacandola07@gmail.com>
2025-07-19 14:53:00 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://todo-list-app-cog.vercel.app'],
+    origin: ['http://localhost:5173', 'https://todo-list-app-pi-vert.vercel.app/'],
   }),
 );
 
